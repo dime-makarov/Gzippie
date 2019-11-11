afd997683c1d2156ba4431688d8ece375c44e5c1
Dmitry Makarov <Dmitry.Makarov.GDC@ts.fujitsu.com>
2019-11-11 15:29:19 +0300

    AssemblyInfo fixed.

--- App/Properties/AssemblyInfo.cs
+++ App/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dm.GZipArchiver.Client")]
+[assembly: AssemblyTitle("Dm.GZippie.App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Dm.GZipArchiver.Client")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dm.GZippie.App")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
