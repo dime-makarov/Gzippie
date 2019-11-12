67a18de51d57db3358a38b76871ff258c21186b6
Dmitry Makarov <Dmitry.Makarov.GDC@ts.fujitsu.com>
2019-11-12 16:26:47 +0300

    Simple command line interface.

--- App/Program.cs
+++ App/Program.cs
@@ -1,18 +1,59 @@
 ﻿using System;
+using CommandLine;
 using Dm.Gzippie.Contract;
 using Dm.Gzippie.Compressor;
 using Dm.Gzippie.Decompressor;
 
 namespace Dm.Gzippie.App
 {
+    public class Options
+    {
+        [Option('m', "mode", Required = true, HelpText = "Mode: Compress|Decompress")]
+        public string Mode { get; set; }
+
+        [Option('s', "src", Required = true, HelpText = "Source file path")]
+        public string SourcePath { get; set; }
+
+        [Option('d', "dest", Required = true, HelpText = "Destination file path")]
+        public string DestinationPath { get; set; }
+    }
+
     public class Program
     {
         static int Main(string[] args)
         {
             try
             {
-                Program app = new Program();
-                app.Run();
+                Options options = null;
+
+                Parser.Default.ParseArguments<Options>(args)
+                    .WithParsed<Options>((opts) =>
+                    {
+                        options = opts;
+                    });
+
+                if (options != null)
+                {
+                    Program app = new Program();
+                    
+                    if (options.Mode.Equals("Compress", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        app.Compress(options.SourcePath, options.DestinationPath);
+                    }
+                    else if (options.Mode.Equals("Decompress", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        app.Decompress(options.SourcePath, options.DestinationPath);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Available values for Mode: Compress|Decompress");
+                        return 1;
+                    }
+                }
+                else
+                {
+                    return 1;
+                }
             }
             catch (Exception ex)
             {
@@ -23,20 +64,16 @@ namespace Dm.Gzippie.App
             return 0;
         }
 
-        public virtual void Run()
+        public virtual void Compress(string srcPath, string destPath)
         {
-            //string srcPath = @"D:\Dox\bookmarks.html";
-            //string destPath = @"D:\Dox\bookmarks_compressed.html";
-            //
-            //using (ICompressor cmpr = new GZipCompressor())
-            //{
-            //    cmpr.Compress(srcPath, destPath);
-            //}
+            using (ICompressor cmpr = new GZipCompressor())
+            {
+                cmpr.Compress(srcPath, destPath);
+            }
+        }
 
-
-            string srcPath = @"D:\Dox\bookmarks_compressed.html";
-            string destPath = @"D:\Dox\bookmarks_decompressed.html";
-            
+        public virtual void Decompress(string srcPath, string destPath)
+        {
             using (IDecompressor dcmpr = new GZipDecompressor())
             {
                 dcmpr.Decompress(srcPath, destPath);
