c762fa5a90dfa71c46548543c69dfbd8853cf956
Dmitry Makarov <to.dime@gmail.com>
2020-06-11 00:33:22 +0300

    Local contracts are nested classes now.

--- Compressor/GZipCompressor.cs
+++ Compressor/GZipCompressor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Text;
 using System.Threading;
 using Dm.Gzippie.Contract;
 
@@ -247,6 +248,67 @@ namespace Dm.Gzippie.Compressor
                 }
 
                 _blocks = null;
+            }
+        }
+
+        private class CompressBlockInfo
+        {
+            /// <summary>
+            /// Sequence number of block (starts from zero)
+            /// </summary>
+            public long SequenceNumber;
+
+            /// <summary>
+            /// Start position in original file
+            /// </summary>
+            public long StartPosition;
+
+            /// <summary>
+            /// Block size in bytes
+            /// </summary>
+            public long OriginalSizeInBytes;
+
+            /// <summary>
+            /// Compressed block size in bytes
+            /// </summary>
+            public long CompressedSizeInBytes;
+
+            /// <summary>
+            /// Path to source file
+            /// </summary>
+            public string SrcPath;
+
+            /// <summary>
+            /// Path to destination file
+            /// </summary>
+            public string DestPath;
+
+            /// <summary>
+            /// Path to the file for gzipped block content
+            /// </summary>
+            public string TempPath;
+
+            /// <summary>
+            /// Raised when block is processed
+            /// </summary>
+            public ManualResetEvent BlockProcessedEvent;
+
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+
+                sb.AppendLine("[");
+                sb.AppendLine($"    {nameof(SequenceNumber)}: {SequenceNumber}");
+                sb.AppendLine($"    {nameof(StartPosition)}: {StartPosition}");
+                sb.AppendLine($"    {nameof(OriginalSizeInBytes)}: {OriginalSizeInBytes}");
+                sb.AppendLine($"    {nameof(CompressedSizeInBytes)}: {CompressedSizeInBytes}");
+                sb.AppendLine($"    {nameof(SrcPath)}: {SrcPath}");
+                sb.AppendLine($"    {nameof(DestPath)}: {DestPath}");
+                sb.AppendLine($"    {nameof(TempPath)}: {TempPath}");
+                sb.AppendLine("]");
+
+                return sb.ToString();
             }
         }
     }
--- Decompressor/GZipDecompressor.cs
+++ Decompressor/GZipDecompressor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Text;
 using System.Threading;
 using Dm.Gzippie.Contract;
 
@@ -231,6 +232,39 @@ namespace Dm.Gzippie.Decompressor
                 }
 
                 _blocks = null;
+            }
+        }
+
+        private class DecompressBlockInfo
+        {
+            public string SrcPath;
+
+            public string DestPath;
+
+            public string TempPath1;
+
+            public string TempPath2;
+
+            public long StartPosition;
+
+            public long SizeInBytes;
+
+            public ManualResetEvent BlockProcessedEvent;
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+
+                sb.AppendLine("[");
+                sb.AppendLine($"    {nameof(SrcPath)}: {SrcPath}");
+                sb.AppendLine($"    {nameof(DestPath)}: {DestPath}");
+                sb.AppendLine($"    {nameof(TempPath1)}: {TempPath1}");
+                sb.AppendLine($"    {nameof(TempPath2)}: {TempPath2}");
+                sb.AppendLine($"    {nameof(StartPosition)}: {StartPosition}");
+                sb.AppendLine($"    {nameof(SizeInBytes)}: {SizeInBytes}");
+                sb.AppendLine("]");
+
+                return sb.ToString();
             }
         }
     }
